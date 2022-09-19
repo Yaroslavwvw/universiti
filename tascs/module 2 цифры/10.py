31f10e40376520aff21c7b87fd4852807605e0de
Yaroslavwvw <gofman05@ya.ru>
2022-09-19 21:03:48 +0300

    new

--- tascs/module 2 цифры/10.py
+++ tascs/module 2 цифры/10.py
@@ -1,18 +1,4 @@
 a = int(input())
 b = int(input())
 c = int(input())
-k = 0
-if a % 2 == 0:
-    k += a // 2
-else:
-    k += a // 2 + 1
-if b % 2 == 0:
-    k += b // 2
-else:
-    k += b // 2 + 1
-if c % 2 == 0:
-    k += c // 2
-else:
-    k += c // 2 + 1
-
-print(k)
+print((a + 1) // 2 + (b + 1) // 2 + (c + 1) // 2)
