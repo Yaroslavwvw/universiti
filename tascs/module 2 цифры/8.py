e894644b90868c3512ade1308f1e3996190dccb1
Yaroslavwvw <gofman05@ya.ru>
2022-09-19 20:56:53 +0300

    new

--- tascs/module 2 цифры/8.py
+++ tascs/module 2 цифры/8.py
@@ -1,17 +1,3 @@
 a = int(input())
-b = a % 7
-if b == 0:
-    print(3)
-elif b == 1:
-    print(4)
-elif b == 2:
-    print(5)
-elif b == 3:
-    print(6)
-elif b == 4:
-    print(7)
-elif b == 5:
-    print(1)
-elif b == 6:
-    print(2)
+print(((a % 7) + 3) % 7)
 
