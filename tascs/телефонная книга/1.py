da29e85160b653b63329ee8b89bcb3e200756477
Yaroslavwvw <gofman05@ya.ru>
2022-10-12 10:08:41 +0300

    news

--- tascs/телефонная книга/1.py
+++ tascs/телефонная книга/1.py
@@ -27,12 +27,44 @@ def get_contact(dict, name, nam):
     return dict
 
 def show_contact(dict):
-    print()
+    print("Список контактов:")
     for i in dict:
         print(i, dict[i])
 
 def menu():
-    print("Выберите действие: \n1. Добавить контакт\n2.Показать список\n3. Выход")
+    print("Выберите действие: \n1. Добавить контакт\n2. Показать список\n3. Выход\n4. Удалить контакт\n5. Редактировать номер")
+
+def dell():
+    name = input("введите имя контакта: ")
+    name = name.title()
+    name = name.lstrip()
+    name = name.rstrip()
+    del dict1[name]
+    print("Контакт успешно удалён!")
+    return dict1
+def fix_numb():
+    name = input("введите имя контакта: ")
+    name = name.title()
+    name = name.lstrip()
+    name = name.rstrip()
+    nam = input("Введите новый номер телефона: ")
+    nam = nam.replace(' ', '').replace('-', '')
+    if nam[0] == '9' and len(nam) == 10:
+        nam = '+7' + nam
+    if nam[0] == '8' and len(nam) == 11:
+        nam = '+7' + nam[1:]
+    if nam[0] == '8' and len(nam) == 11:
+        nam = '+' + nam
+    if nam[:2] == '+7' and len(nam) == 12:
+        dict1[name] = nam
+        print("Номер успешно изменён!")
+        return dict1
+    else:
+        print("Неправильно набран номер")
+        return get_nam()
+
+
+
 
 dict1 = {}
 while True:
@@ -43,4 +75,10 @@ while True:
     if  p == 2:
         show_contact(dict1)
     if  p == 3:
-        get_contact("Спасибо за использование!")
+        print(("Спасибо за использование!"))
+        break
+        #get_contact("Спасибо за использование!")
+    if p == 4:
+        dell()
+    if p == 5:
+        fix_numb()
