dae3eeeb1d76b05357dbe4376852efe4a0c8ff62
Yaroslavwvw <gofman05@ya.ru>
2022-11-15 23:08:45 +0300

    news

--- tascs/praktika 4/1.py
+++ tascs/praktika 4/1.py
@@ -1,19 +1,35 @@
 F = open('data.txt', 'r', encoding='UTF-8')
 st = list(map(str, F.readlines()))
 #print(st)
-ss = '/?.,@#$^%&*()_-!"№;;%:?*<>|'
+#ss = '/?.,@#$^%&*()_-!"№;;%:?*<>|'
 ss1 = 'йцукенгшщзхъфывапролджэячсмитьбюЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ'
 new = []
 
-for i in range(len(st)):
-    for j in st[i]:
-        if j not in ss1:
-            st[i] = st[i].replace(j, ' ')
-    st[i] = st[i].lower()
-    st[i] = st[i].split()
-    for j in range(len(st[i])):
-        if st[i][j]+'\n' not in new:
-            new.append(st[i][j]+'\n')
+def clear_sort(st):
+    for i in range(len(st)):
+        for j in st[i]:
+            if j not in ss1:
+                st[i] = st[i].replace(j, ' ')
+        st[i] = st[i].lower()
+        st[i] = st[i].split()
+        for j in range(len(st[i])):
+            if st[i][j]+'\n' not in new:
+                new.append(st[i][j]+'\n')
+    return new
 
-print(st)
-print(new)
+clear_sort(st)
+
+#!/usr/bin/env python
+# -* coding: utf-8 -*-
+
+#print(sys.version)
+import sys
+new_text = open("new_text.txt", mode='w')
+new_text.write(f'Всего уникальных слов: {len(new)}\n')
+new_text.write('\n')
+for i in sorted(new):
+    new_text.write(i)
+print()
+#print(st)
+#print()
+#print(new)
---
+++ tascs/praktika 4/demo 2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -* coding: utf-8 -*-
+d = [u'ё', u'ель', u'ёлочка', u'ёжик', u'ё моё', u'е',  u'ельник', u'ежовый']
+import sys
+print (sys.version)
+for i in sorted(d):
+    print i
